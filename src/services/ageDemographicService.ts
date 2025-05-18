94ef81189ff815b8fc60a2106219b76a1924c0ac
Shawn Koski <shawnk@aplos.com>
2025-05-18 03:28:09 +0200

    feat: age demographics service

---
+++ src/services/ageDemographicService.ts
@@ -0,0 +1,90 @@
+import { AgeDemographic } from '@prisma/client';
+import { AgeDemographicFormData } from '../types';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+export const fetchAllAgeDemographics = async (): Promise<AgeDemographic[]> => {
+  const response = await fetch(`${API_BASE_URL}ageDemographics`);
+  if (!response.ok) {
+    throw new Error(`In fetchAllAgeDemographics - HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data as AgeDemographic[];
+};
+
+export const fetchCityAgeDemographics = async (cityId: number): Promise<AgeDemographic[]> => {
+  const response = await fetch(`${API_BASE_URL}ageDemographics?cityId=${cityId}`);
+  if (!response.ok) {
+    throw new Error(`In fetchCityAgeDemographics - HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data as AgeDemographic[];
+};
+
+export const createAgeDemographic = async (
+  AgeDemographicData: AgeDemographicFormData
+): Promise<AgeDemographic> => {
+  const response = await fetch(`${API_BASE_URL}ageDemographics`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(AgeDemographicData)
+  });
+  if (!response.ok) {
+    let errorData;
+    try {
+      errorData = await response.json();
+    } catch {
+      throw new Error(
+        `In createAgeDemographic - HTTP error status: ${response.status}, message: ${
+          errorData?.message || response.statusText
+        }`
+      );
+    }
+  }
+
+  const data = await response.json();
+  return data as AgeDemographic;
+};
+
+export const updateAgeDemographic = async (
+  AgeDemographicData: AgeDemographicFormData,
+  id: number
+): Promise<AgeDemographic> => {
+  const response = await fetch(`${API_BASE_URL}ageDemographics/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(AgeDemographicData)
+  });
+  if (!response.ok) {
+    let errorData;
+    try {
+      errorData = await response.json();
+    } catch {
+      throw new Error(
+        `In updateAgeDemographic - HTTP error status: ${response.status}, message: ${
+          errorData?.messages || response.statusText
+        }`
+      );
+    }
+  }
+
+  const data = await response.json();
+  return data as AgeDemographic;
+};
+
+export const deleteAgeDemographic = async (id: number): Promise<void> => {
+  const response = await fetch(`${API_BASE_URL}ageDemographics/${id}`, {
+    method: 'DELETE'
+  });
+  if (!response.ok) {
+    throw new Error(
+      `In deleteAgeDemographic - HTTP error! status: ${response.status} ${response.statusText}`
+    );
+  }
+};
