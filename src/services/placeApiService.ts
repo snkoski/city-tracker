7a708fdabc58f8d2112ff1a8fb2c054f2658d6db
Shawn Koski <shawnk@aplos.com>
2025-05-18 03:22:33 +0200

    feat: place api service

---
+++ src/services/placeApiService.ts
@@ -0,0 +1,85 @@
+import { Place } from '@prisma/client';
+import { PlaceFormData } from '../types';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+export const fetchAllPlaces = async (): Promise<Place[]> => {
+  const response = await fetch(`${API_BASE_URL}/places`);
+  if (!response.ok) {
+    throw new Error(`In fetchAllPlaces - HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data as Place[];
+};
+
+export const fetchCityPlaces = async (cityId: number): Promise<Place[]> => {
+  const response = await fetch(`${API_BASE_URL}/places?cityId=${cityId}`);
+  if (!response.ok) {
+    throw new Error(`In fetchCityPlaces - HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data as Place[];
+};
+
+export const createPlace = async (PlaceData: PlaceFormData): Promise<Place> => {
+  const response = await fetch(`${API_BASE_URL}/places`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(PlaceData)
+  });
+  if (!response.ok) {
+    let errorData;
+    try {
+      errorData = await response.json();
+    } catch {
+      throw new Error(
+        `In createEventPlace - HTTP error status: ${response.status}, message: ${
+          errorData?.message || response.statusText
+        }`
+      );
+    }
+  }
+
+  const data = await response.json();
+  return data as Place;
+};
+
+export const updatePlace = async (PlaceData: PlaceFormData, id: number): Promise<Place> => {
+  const response = await fetch(`${API_BASE_URL}/places/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(PlaceData)
+  });
+  if (!response.ok) {
+    let errorData;
+    try {
+      errorData = await response.json();
+    } catch {
+      throw new Error(
+        `In updatePlace - HTTP error status: ${response.status}, message: ${
+          errorData?.messages || response.statusText
+        }`
+      );
+    }
+  }
+
+  const data = await response.json();
+  return data as Place;
+};
+
+export const deletePlace = async (id: number): Promise<void> => {
+  const response = await fetch(`${API_BASE_URL}/places/${id}`, {
+    method: 'DELETE'
+  });
+  if (!response.ok) {
+    throw new Error(
+      `In deletePlace - HTTP error! status: ${response.status} ${response.statusText}`
+    );
+  }
+};
