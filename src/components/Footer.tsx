3a3588bfbb84bf9b00062ac02b4c773bf3e7f849
Shawn Koski <shawnk@aplos.com>
2025-05-22 01:31:55 +0200

    feat: footer text update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ export const Footer = () => {
   return (
     <footer className="mt-auto w-full bg-pink-400 text-lime-300 items-center">
       <p className="text-center">
-        Made by real fingers &copy; {currentYear} Cityr Trackerly. No rights reserved, feel free to
-        fuck this shit up lol.
+        Made by real fingers (what a concept) &copy; {currentYear} Cityr Trackerly. No rights
+        reserved, feel free to fuck this shit up lol.
       </p>
     </footer>
   );
