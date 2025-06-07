376f783e523cff3fb952b06359cf2c8d19d123e2
Shawn Koski <shawnnkoski@gmail.com>
2025-06-07 09:50:30 +0200

    feat: prisma migrations

---
+++ prisma/migrations/20250606213417_change_remaining_decimals_to_strings/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "cities" ALTER COLUMN "sales_tax_rate" SET DATA TYPE TEXT,
+ALTER COLUMN "property_crime_rate" SET DATA TYPE TEXT,
+ALTER COLUMN "violent_crime_rate" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "states" ALTER COLUMN "income_tax_rate" SET DATA TYPE TEXT;
